1e4cdb017f674777ac7be7c4aa1278c0a74bea63
UserPL022Pc15 <UserPL022Pc15@SLFRPL022PC15.ensino.atec.local>
2017-01-31 07:53:36 +0000

    commit final

---
+++ FTP-Cliente/src/ftp/cliente/Main.java
@@ -0,0 +1,17 @@
+package ftp.cliente;
+
+import java.io.IOException;
+import java.util.Scanner;
+import pt.atec.network.ClienteArray;
+import pt.atec.network.ClienteFile;
+import pt.atec.network.ServerCom;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        ServerCom sc = new ServerCom();
+        
+        ClienteFile cf = new ClienteFile();
+    }
+
+}
--- FTP-Cliente/src/pt/atec/network/ClienteNome.java
+++ FTP-Cliente/src/pt/atec/network/ClienteNome.java
@@ -3,24 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package network;
+package pt.atec.network;
 
 import java.io.DataInputStream;
 import java.net.Socket;
 
-public class ClienteCom {
+public class ClienteNome {
     private final int PORTO = 80;
     private final String IP = "192.168.250.250";
-    public int msgin = 0;
+    public String msgin = "";
 
-    public ClienteCom() {
+    public ClienteNome() {
         try {
 
             Socket sock = new Socket(IP, PORTO);
 
             DataInputStream din = new DataInputStream(sock.getInputStream());
 
-            msgin = din.read();
+            msgin = din.readUTF(); 
 
         } catch (Exception e) {
 
